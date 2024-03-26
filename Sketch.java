5b1027d14a69e5c0329204bb0cad7162153291d2
Erick-Meza <erick.mezavilleda25@ycdsbk12.ca>
2024-03-26 18:46:10 -0400

    Completed Section 6

--- Sketch.java
+++ Sketch.java
@@ -123,14 +123,68 @@ public class Sketch extends PApplet {
 
   // Creates a method for the third quadrant of the image
   public void draw_section4() {
+
+    // Creates a Nested loop to create pixel drawing
+    for (int i = 0; i < 30; i++) {
+      for (int j = 0; j < 30; j++) {
+
+        // Creates a Conditional Selective Algorithm to determine pixel colour
+        if (i % 2 == 0) {
+          fill(0, 0, 0);
+        } else if (i % 2 != 0) {
+          if (j % 2 == 0) {
+            fill(255, 255, 255);
+          } else if (j % 2 != 0) {
+            fill(0, 0, 0);
+          }
+        }
+
+        // Initializes cvariables for the coordinates
+        int intXCoordinate = 904 + j * 5 * 2;
+        int intYCoordinate = 304 + i * 5 * 2;
+
+        // Outputs the pixels onto the screen
+        rect(intXCoordinate, intYCoordinate, 5, 5);
+      }
+    }
   }
 
   // Creates a method for the fifth quadrant of the image
   public void draw_section5() {
+
+    // Initializes variables
+    int intPixelNumber = 10;
+
+    // Creates a Nested loop to print the right amount of pixels
+    for (int i = 594; i >= intPixelNumber; i -= 10) {
+      for (int j = 292; j >= intPixelNumber; j -= 10) {
+
+        // Prints out the pixels in quadrant 5
+        fill(255, 255, 255);
+        rect(j, i - 300, 5, 5);
+      }
+
+      // Reassigns the value of variables
+      intPixelNumber += 10;
+    }
   }
 
   // Creates a method for the sixth quadrant of the image
   public void draw_section6() {
+
+    // Initializes variables
+    int intPixelNumber = 600;
+
+    // Creates a Nested Loop to output pixels
+    for (int i = 900; i >= intPixelNumber; i -= 10) {
+      for (int j = 302; j <= intPixelNumber; j += 10) {
+        fill(255, 255, 255);
+        rect(j, i - 606, 5, 5);
+      }
+
+      // Reassigns values of variables
+      intPixelNumber -= 10;
+    }
   }
 
   // Creates a method for the seventh quadrant of the image
